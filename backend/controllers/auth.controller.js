00fe28ea7fcd6866332c06ffcd68e8b3c03fbeb2
tox-in <habimanat78@gmail.com>
2024-07-30 21:52:53 +0200

    login functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -62,7 +62,38 @@ export async function signup(req, res) {
 }
 
 export async function login(req, res) {
-    res.send("Login route")
+    try {
+        const {email, password} = req.body;
+
+        if(!email || !password) {
+            return res.status(400).json({success:false,message:"Email and password are required"});
+        
+        }
+        
+        const user = await User.findOne({email:email});
+        if(!user) {
+            return res.status(400).json({success:false,message:"Invalid credentials"});
+        }
+
+        const isPasswordCorrect = await bcryptjs.compare(password, user.password);
+
+        if(!isPasswordCorrect) {
+            return res.status(400).json({success:false,message:"Invalid credentials"});
+        }
+
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(200).json({
+            success:true,
+            user:{
+                ...user._doc,
+                password: "",
+            }
+        })
+    } catch(error) {
+        console.log("Error logging in", error.message);
+        res.status(500).json({success:false, message:"Internal server error"})
+    }
 }
 
 export async function logout(req, res) {
