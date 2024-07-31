6b1d5a395cc2a5e3cc012a49b3e102aee3cf8f08
tox-in <habimanat78@gmail.com>
2024-07-31 20:24:16 +0200

    get Similar Movies

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -40,3 +40,18 @@ export async function getMovieDetails(req, res) {
         res.status(500).json({ success: false, message: "Internal server error" });
     }
 }
+
+export async function getSimilarMovies(req, res) {
+    const { id } = req.params;
+
+    try {
+        const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${id}/similar?language=en-US`);
+        res.json({ success: true, content: data.results });
+    } catch(error) {
+        if(error.message.includes("404")){
+            return res.status(404).send(null)
+        }
+
+        res.status(500).json({ success: false, message: "Internal server error"});
+    }
+}
--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getTrendingMovies, getMovieTrailers, getMovieDetails } from "../controllers/movie.controller.js";
+import { getTrendingMovies, getMovieTrailers, getMovieDetails, getSimilarMovies } from "../controllers/movie.controller.js";
 
 const router = express.Router();
 
 router.get("/trending",getTrendingMovies);
 router.get("/:id/trailers", getMovieTrailers);
 router.get("/:id/details", getMovieDetails);
+router.get("/:id/similar", getSimilarMovies);
 
 export default router;
