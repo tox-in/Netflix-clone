0c99428cb76a6e62ed37377cff59e526793e1ede
tox-in <habimanat78@gmail.com>
2024-08-06 11:35:33 +0200

    Frontend Initials

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+content: [
+  "./index.html",
+  "./src/**/*.{html,js}"
+],
+theme: {
+  extend: {},
+},
+plugins: [],
 }
-
