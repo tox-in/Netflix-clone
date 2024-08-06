721654b04efae83a3880d750ed3e302f1ffdb651
tox-in <habimanat78@gmail.com>
2024-08-06 12:22:58 +0200

    added assets and new features

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline text-red-600">Hello world!</h1>
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage/>} />
+      <Route path="/login" element={<LoginPage/>} />
+      <Route path="/signup" element={<SignUpPage/>} />
+    </Routes>
+  )
 }
 
 export default App
