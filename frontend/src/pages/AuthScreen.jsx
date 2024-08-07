8e8e2a7032782ffcc35a9b10afa217a46afed44a
tox-in <habimanat78@gmail.com>
2024-08-07 14:55:26 +0200

    email pass

--- frontend/src/pages/AuthScreen.jsx
+++ frontend/src/pages/AuthScreen.jsx
@@ -1,9 +1,16 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { ChevronRight } from "lucide-react"
 
 const AuthScreen = () => {
     const [email, setEmail] = useState("");
+    const navigate = useNavigate();
+
+    const handleFormSubmit = (e) => {
+      e.preventDefault();
+      navigate("/signup?email=" + email);
+    };
+
   return (
     <div className="hero-bg relative">
       {/* Navbar */}
@@ -20,7 +27,7 @@ const AuthScreen = () => {
         <p className="text-lg mb-4">Watch anywhere. Cancel anytime.</p>
         <p className="mb-4">Ready to watch? Enter your email to create or restart your membership.</p>
 
-        <form className="flex flex-col md:flex-row gap-4 w-1/2">
+        <form className="flex flex-col md:flex-row gap-4 w-1/2" onSubmit={handleFormSubmit}>
         <input
          type="email"
          placeholder="Email address" 
