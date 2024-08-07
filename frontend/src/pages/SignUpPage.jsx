2cecad39c4a59729be47bf477dac9f0afa4e79ec
tox-in <habimanat78@gmail.com>
2024-08-07 10:12:06 +0200

    Login and Signup pages

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,6 +1,15 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const SignUpPage = () => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    console.log(email, username, password);
+  }
   return (
     <div className="h-screen w-full hero-bg">
       <header className="max-w-6xl mx-auto flex items-center justify-between p-4">
@@ -13,29 +22,53 @@ const SignUpPage = () => {
         <div className="w-full max-w-md p-8 space-y-6 bg-black/60 rounded-lg shadow-md">
           <h1 className="text-center text-white text-2xl font-bold mb-4">Sign Up</h1>
 
-          <form className='space-y-2'>
+          <form className='space-y-2' onSubmit={handleSignUp}>
             <div>
               <label htmlFor="email" className='text-sm font-medium text-gray-300 block'>
                 Email
               </label>
-              <input type="email" className="w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring" placeholder='your@email.com' />
+              <input
+               type="email"
+               className="w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white
+               focus:outline-none focus:ring"
+               placeholder='your@email.com'
+               id='email'
+               value={email}
+               onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
 
             <div>
               <label htmlFor="username" className='text-sm font-medium text-gray-300 block'>
                 Username
               </label>
-              <input type="text" className="w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring" placeholder='yourusername' />
+              <input
+               type="text"
+               className="w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white
+               focus:outline-none focus:ring"
+               placeholder='yourusername'
+               id='username'
+               value={username}
+               onChange={(e) => setUsername(e.target.value)}
+               />
             </div>
             
             <div>
               <label htmlFor="password" className='text-sm font-medium text-gray-300 block'>
                 Password
               </label>
-              <input type="password" className="w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring" placeholder='yourpassword' />
+              <input
+               type="password"
+               className="w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white
+               focus:outline-none focus:ring"
+               placeholder='yourpassword'
+               id='password'
+               value={password}
+               onChange={(e) => setPassword(e.target.value)}
+               />
             </div>
 
-            <button className='w-full py-2 bg-red-600 text-white font-semiblold rounded-md hover:bg-red-700'>
+            <button className='w-full py-2 bg-red-600 text-white font-semibold rounded-md hover:bg-red-700'>
               Sign Up
             </button>
           </form>
