0c321f052682f591afa551732ce92e83682116af
tox-in <habimanat78@gmail.com>
2024-08-12 20:28:16 +0200

    authCheck

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,40 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import Footer from "./components/Footer";
 import { useAuthStore } from "./store/authUser";
 import { useEffect } from "react";
+import { Loader } from "lucide-react";
 
 function App() {
   const { user, isCheckingAuth, authCheck } = useAuthStore();
-  console.log("auth user is here: ", user);
+  console.log("isCheckingAuth: ", isCheckingAuth);
 
   useEffect(() => {
     authCheck();
-  }, []);
+  }, [authCheck]);
+
   return (
     <>
-    <Routes>
-      <Route path="/" element={<HomePage/>} />
-      <Route path="/login" element={<LoginPage/>} />
-      <Route path="/signup" element={<SignUpPage/>} />
-    </Routes>
-    <Footer />
+      {isCheckingAuth ? (
+        <div className="h-screen">
+          <div className="flex justify-center items-center bg-black h-full">
+            <Loader className="animate-spin text-red-600 size-10" />
+          </div>
+        </div>
+      ) : (
+        <>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={!user ? <LoginPage /> : <Navigate to="/" />} />
+            <Route path="/signup" element={!user ? <SignUpPage /> : <Navigate to="/" />} />
+          </Routes>
+          <Footer />
+        </>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,18 +1,9 @@
-import { useEffect } from 'react';
 import { useAuthStore } from "../store/authUser";
 import AuthScreen from "./AuthScreen";
 import HomeScreen from "./HomeScreen";
 
 const HomePage = () => {
-    const { user, authCheck, isCheckingAuth } = useAuthStore();
-
-    useEffect(() => {
-        authCheck();
-    }, [authCheck]);
-
-    if (isCheckingAuth) {
-        return <div>Loading...</div>;
-    }
+    const { user } = useAuthStore();
 
     return <div>{user ? <HomeScreen /> : <AuthScreen />}</div>;
 };
