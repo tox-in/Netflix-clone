2c4b91f5d13fc1db97fa669a09103bb0f67fb182
tox-in <habimanat78@gmail.com>
2024-08-14 16:41:21 +0200

    changes on homeScreen

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,60 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { LogOut, Search, Menu } from "lucide-react";
+import { useAuthStore } from "../store/authUser";
 
 const Navbar = () => {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+    const { user, logout } = useAuthStore()
+
+    const toggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
   return (
     <header className='max-w-6xl mx-auto flex flex-wrap items-center justify-between p-4 h-20'>
       <div className="flex items-center gap-10 z-50">
         <Link to="/">
             <img src="/netflix-logo.png" alt="netflix logo" className="w-32 sm:w-40" />
         </Link>
+
+        {/* desktop navbar items */}
+        <div className="hidden sm:flex gap-2 items-center">
+            <Link to="/" className="hover-underline">
+                Movies
+            </Link>
+            <Link to="/" className="hover-underline">
+                Tv Shows
+            </Link>
+            <Link to="/history" className="hover-underline">
+                Search History
+            </Link>
+        </div>
       </div>
+
+      <div className="flex gap-2 items-center z-20">
+        <Link to={"/search"}>
+        <Search className="size-6 cursor-pointer" />
+        </Link>
+        <img src={user.image} alt="avatar" className="h-8 rounded cursor-pointer" />
+        <LogOut className="size-6 cursor-pointer" onClick={logout} />
+
+        <div className="sm:hidden">
+           <Menu className="size-6 cursor-pointer" onClick={toggleMobileMenu}></Menu> 
+        </div>
+      </div>
+
+      {/* mobile navbar items */}
+      {isMobileMenuOpen && (
+        <div className="w-full flex sm:hidden mt-4 z-50 bg-black border rounded border-gray-800 flex-col">
+            <Link to={"/"} className="black hover-underline p-2" onClick={toggleMobileMenu}>
+               Movies 
+            </Link>
+            <Link to={"/"} className="black hover-underline p-2" onClick={toggleMobileMenu}>
+               Tv Shows 
+            </Link>
+            <Link to={"/history"} className="black hover-underline p-2" onClick={toggleMobileMenu}>
+               Search History
+            </Link>
+        </div>
+      )}
     </header>
   )
 }
