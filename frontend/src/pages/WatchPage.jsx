2529db03d4382063aca1207e1133ea00e6976ff2
tox-in <habimanat78@gmail.com>
2024-08-16 19:43:50 +0200

    change

--- frontend/src/pages/WatchPage.jsx
+++ frontend/src/pages/WatchPage.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useContentStore } from '../store/content';
+import Navbar from '../components/Navbar';
 import axios from 'axios';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
 
 const WatchPage = () => {
     const { id } = useParams();
@@ -61,11 +63,44 @@ const WatchPage = () => {
     }, [contentType,id]);
 
     console.log("contentDetails", content);
-    
-    
+
+    const handleNext = () => {
+        if (currentTrailerIdx < trailers.length - 1)
+            setCurrentTrailerIdx(currentTrailerIdx + 1);
+    }
+    const handlePrev = () => {
+        if (currentTrailerIdx > 0)
+        setCurrentTrailerIdx(currentTrailerIdx - 1)
+    }
   return (
-    <div>
-      Watch Page
+    <div className='bg-black min-h-screen text-white'>
+      <div className="mx-auto container px-4 py-8 h-full">
+        <Navbar />
+
+        {trailers.length > 0 && (
+            <div className="flex justify-between items-center mb-4">
+                <button className={`
+                    bg-gray-500/70 hover:bg-gray-500 text-white py-2 px-4 rounded ${currentTrailerIdx === 0 ? 
+                    'opacity-50 cursor-not-allowed' : ""}
+                    `}
+                    disabled={currentTrailerIdx === 0}
+                    onClick={handlePrev}
+                    >
+                    <ChevronLeft size={24} />
+                </button>
+
+                <button className={`
+                    bg-gray-500/70 hover:bg-gray-500 text-white py-2 px-4 rounded ${currentTrailerIdx === trailers.length - 1 ? 
+                    'cursor-not-allowed opacity-50' : ""}
+                    `}
+                    disabled={currentTrailerIdx === trailers.length -1 }
+                    onClick={handleNext}
+                    >
+                    <ChevronRight size={24} />
+                </button>
+            </div>
+        )}
+      </div>
     </div>
   )
 }
