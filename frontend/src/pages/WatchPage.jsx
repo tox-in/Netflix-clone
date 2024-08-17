e13441c902a419c8e723675d7798d76113685ea3
tox-in <habimanat78@gmail.com>
2024-08-17 16:19:21 +0200

    Watchpage skeleton

--- frontend/src/pages/WatchPage.jsx
+++ frontend/src/pages/WatchPage.jsx
@@ -1,19 +1,14 @@
-import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useEffect, useState, useRef } from 'react'
+import { Link, useParams } from 'react-router-dom'
 import { useContentStore } from '../store/content';
 import Navbar from '../components/Navbar';
 import axios from 'axios';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 import ReactPlayer from "react-player";
-import { ORIGINAL_IMG_BASE_URL } from '../constants/constants';
-
-function formatReleaseDate(date) {
-    return new Date(date).toLocaleDateString("en-US", {
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-    });
-}
+import { ORIGINAL_IMG_BASE_URL, SMALL_IMG_BASE_URL } from '../constants/constants';
+import { formatReleaseDate } from '../../utils/dateFunction';
+import WatchPageSkeleton from '../components/skeletons/WatchPageSkeleton';
+  
 
 const WatchPage = () => {
     const { id } = useParams();
@@ -23,6 +18,8 @@ const WatchPage = () => {
     const [loading, setLoading] = useState(true);
     const [content, setContent] = useState({});
     const {contentType} = useContentStore();
+
+    const sliderRef = useRef(null);
 
     useEffect(() => {
         const getTrailers = async () => {
@@ -38,8 +35,6 @@ const WatchPage = () => {
         getTrailers();
     }, [contentType,id]);
 
-    console.log(trailers);
-
     useEffect(() => {
         const getSimilarContent = async () => {
             try {
@@ -54,8 +49,6 @@ const WatchPage = () => {
         };
         getSimilarContent();
     }, [contentType,id]);
-
-    console.log("similar content", similarContent);
     
     useEffect(() => {
         const getContentDetails = async () => {
@@ -72,8 +65,6 @@ const WatchPage = () => {
         getContentDetails();
     }, [contentType,id]);
 
-    console.log("contentDetails", content);
-
     const handleNext = () => {
         if (currentTrailerIdx < trailers.length - 1)
             setCurrentTrailerIdx(currentTrailerIdx + 1);
@@ -82,6 +73,36 @@ const WatchPage = () => {
         if (currentTrailerIdx > 0)
         setCurrentTrailerIdx(currentTrailerIdx - 1)
     }
+
+    const scrollLeft = () => {
+        if(sliderRef.current){
+            sliderRef.current.scrollBy({left: -sliderRef.current.offsetWidth, behavior: 'smooth'})
+        }
+    }
+    const scrollRight = () => {
+        sliderRef.current.scrollBy({left: sliderRef.current.offsetWidth, behavior: 'smooth'})
+    }
+
+    // if(loading) return (
+    //     <div className="min-h-screen bg-black p-10">
+    //         <WatchPageSkeleton></WatchPageSkeleton>
+    //     </div>
+    // );
+
+    if(!content) {
+        return (
+            <div className="bg-black text-white h-screen">
+                <div className="max-w-6xl mx-auto">
+                    <Navbar />
+                    <div className="text-center mx-auto px-4 py-8 h-full mt-40">
+                        <h2 className="text-2xl sm:text-5xl font-bold text-balance">Content not found </h2>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+
   return (
     <div className='bg-black min-h-screen text-white'>
       <div className="mx-auto container px-4 py-8 h-full">
@@ -148,6 +169,30 @@ const WatchPage = () => {
             </div>
             <img src={ORIGINAL_IMG_BASE_URL + content.poster_path} alt="Poster image" className='max-h-[600px] rounded-md' />
         </div>
+
+        {similarContent.length > 0 && (
+            <div className="mt-12 max-w-5xl mx-auto relative">
+                <h3 className="text-3xl font-bold mb-4">
+                    Similar Movies/Tv Show
+                </h3>
+
+                <div className="flex overflow-x-scroll scrollbar-hide gap-4 pb-4 group" ref={sliderRef}>
+                    {similarContent.map((content) => {
+                       if(content.poster_path === null) return null;
+                       return(
+                        <Link key={content.id} to={`/watch/${content.id}`} className='w-52 flex-none'>
+                            <img src={SMALL_IMG_BASE_URL + content?.poster_path} alt="Poster path" className='w-full h-auto rounded-md' />
+                            <h4 className="mt-2 text-lg font-semibold">{content.title || content.name}</h4>
+                       </Link>
+                       ) 
+                    })}
+                </div>
+            </div>
+        )}
+
+        <ChevronRight className="absolute top-1/2  -translate-y-1/2 right-2 w-8 h-8 opacity-0 group-hover:opacity-100 transition-all duration-300 cursor-pointer bg-red-600 text-white rounded-full" onClick={scrollRight} />
+
+        <ChevronLeft className="absolute top-1/2  -translate-y-1/2 left-2 w-8 h-8 opacity-0 group-hover:opacity-100 transition-all duration-300 cursor-pointer bg-red-600 text-white rounded-full" onClick={scrollLeft} />
 
       </div>
     </div>
