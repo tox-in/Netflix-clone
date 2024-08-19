c9284e52ec49d25edb0872115038b82b4d719837
tox-in <habimanat78@gmail.com>
2024-08-19 15:12:57 +0200

    moved package.json

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -69,7 +69,7 @@ const SearchPage = () => {
                                     <h2 className="mt-2 text-xl font-bold">{result.name}</h2>
                                 </div>
                             ) : (
-                                <Link to={"/watch/" + result.id}>
+                                <Link to={"/watch/" + result.id} onClick={() =>{setContentType(activeTab);}}>
                                     <img src={ORIGINAL_IMG_BASE_URL + result.poster_path} alt={result.title || result.name} className="w-full h-auto rounded" />
                                     <h2 className="mt-2 text-xl font-bold">{result.title || result.name}</h2>
                                 </Link>
