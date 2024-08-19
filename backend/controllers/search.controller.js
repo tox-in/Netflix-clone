a0c0057d7fff4cb4312bc4fe9f3000c059999760
tox-in <habimanat78@gmail.com>
2024-08-19 12:21:07 +0200

    Search Movie

--- backend/controllers/search.controller.js
+++ backend/controllers/search.controller.js
@@ -34,7 +34,7 @@ export async function searchMovie(req, res) {
 
     try {
         const response = await fetchFromTMDB(`https://api.themoviedb.org/3/search/movie?query=${query}&language=en-US&page=1`);
-
+        
         if(response.results.length === 0) {
             return res.status(404).send(null);
         }
@@ -43,8 +43,8 @@ export async function searchMovie(req, res) {
             $push:{
                 searchHistory:{
                     id:response.results[0].id,
-                    image:response.results[0].profile_path,
-                    title:response.results[0].name,
+                    image:response.results[0].backdrop_path,
+                    title:response.results[0].original_title,
                     searchType:"movie",
                     createdAt: new Date()
                 },
