8e0cf3c0398cd4ae21f66a06aa72907b55adc281
tox-in <habimanat78@gmail.com>
2024-08-21 12:00:25 +0200

    reference changes

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom"
 
 const NotFoundPage = () => {
   return (
-    <div className="min-h-screen bg-cover bg-center flex flex-col justify-center items-center text-white" style={{ backgroundImage: `url('../public/404.png')`}}>
+    <div className="min-h-screen bg-cover bg-center flex flex-col justify-center items-center text-white" style={{ backgroundImage: `url('../../public/404.png')`}}>
           <header className="absolute top-0 left-0 p-4 bg-black w-full">
             <Link to={"/"}>
-                <img src='../public/netflix-logo.png' alt="Netflix" className="h-8" />
+                <img src='../../public/netflix-logo.png' alt="Netflix" className="h-8" />
             </Link>
           </header>
           <main className="text-center error-page--content z-10">
