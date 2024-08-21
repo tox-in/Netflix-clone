dbd761b04b86c3c951a13a38f0b79cf0ee56ace2
tox-in <habimanat78@gmail.com>
2024-08-21 09:30:30 +0200

    changes

--- backend/server.js
+++ backend/server.js
@@ -8,22 +8,13 @@ import searchRoutes from "./routes/search.route.js";
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
 import { protectRoute } from "./middleware/protectRoute.js";
+import path from "path" ;
 
-if(ENV_VARS.NODE_ENV === "production") {
-     app.use(express.static(path.join(__dirname, "/frontend/dist")))
-}
 
 const app = express();
-const PORT = ENV_VARS.PORT;
 
-const corsOptions = {
-     origin: 'http://localhost:5173',
-     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-     credentials: true,
-     optionsSuccessStatus: 204
- };
- 
-app.use(cors(corsOptions));
+const PORT = ENV_VARS.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -33,6 +24,14 @@ app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
 app.use("/api/v1/search", protectRoute, searchRoutes);
 
+if(ENV_VARS.NODE_ENV === "production") {
+     app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+
+     app.get("*", (req, res) => {
+          res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+     })
+}
 
 app.listen(PORT, ()=>{
      console.log(`server started on http://localhost:${PORT}`);
