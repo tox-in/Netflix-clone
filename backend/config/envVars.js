3590e7090e4b9677be2cd62e24810acc1e59e602
tox-in <habimanat78@gmail.com>
2024-08-21 15:50:36 +0200

    flexflix server

--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -10,7 +10,7 @@ dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 export const ENV_VARS = {
     MONGO_URI: process.env.MONGO_URI,
-    PORT: process.env.PORT || 5020,
+    PORT: process.env.PORT,
     JWT_SECRET: process.env.JWT_SECRET,
     NODE_ENV: process.env.NODE_ENV,
     TMDB_API_KEY: process.env.TMDB_API_KEY
